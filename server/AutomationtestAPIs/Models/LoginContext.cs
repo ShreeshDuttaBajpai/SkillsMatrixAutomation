0852f8006b9a2ea59c4a1ea1703ee4c012fbd120
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2022-11-22 18:13:25 +0530

    backend base structure

---
+++ server/AutomationtestAPIs/Models/LoginContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomationtestAPIs.Models
+{
+    public class LoginContext : DbContext
+    {
+        public LoginContext()
+        {
+        }
+        public LoginContext(DbContextOptions<LoginContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Login> UserModels { get; set; }
+        public virtual DbSet<StoryTrackerDB> StoryTrackerDBs { get; set; }
+    }
+}
