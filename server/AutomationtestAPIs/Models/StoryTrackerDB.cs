c5d7c3cc0ff0d67a16ab8d0d5c1f1b88b5577f60
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2022-11-24 12:09:40 +0530

    backend datatype change

--- server/AutomationtestAPIs/Models/StoryTrackerDB.cs
+++ server/AutomationtestAPIs/Models/StoryTrackerDB.cs
@@ -17,8 +17,8 @@ namespace AutomationtestAPIs.Models
         public string Ticket_no { get; set; }
         public string Ticket_type { get; set; }
         public int Story_point { get; set; }
-        public string Start_date { get; set; }
-        public string End_date { get; set; }
+        public DateTime Start_date { get; set; }
+        public DateTime End_date { get; set; }
         public int Hours { get; set; }
         public string Status { get; set; }
         public string Code_reviewer { get; set; }
