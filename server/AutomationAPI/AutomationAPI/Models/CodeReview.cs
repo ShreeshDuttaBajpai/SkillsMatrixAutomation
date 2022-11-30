ef2a67155c1d57d553a437e24ab1f2f200d5f4dc
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2022-11-30 14:39:15 +0530

    Backend Base structure on dotnet6

---
+++ server/AutomationAPI/AutomationAPI/Models/CodeReview.cs
@@ -0,0 +1,10 @@
+﻿namespace AutomationAPI.Models
+{
+    public class CodeReview
+    {
+        public string? Code_reviewer { get; set; }
+        public string? Code_deviation_count { get; set; }
+        public string? Bugs_count { get; set; }
+        public string? Remarks { get; set; }
+    }
+}
--- server/AutomationAPI/AutomationAPI/Models/StoryTrackerDB.cs
+++ server/AutomationAPI/AutomationAPI/Models/StoryTrackerDB.cs
@@ -1,10 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutomationAPI.Models
 {
     public class StoryTrackerDB
     {
-        public int Serial_no { get; set; }
         public string Client { get; set; }
         public string Team { get; set; }
         public string Name { get; set; }
@@ -18,8 +18,5 @@ namespace AutomationAPI.Models
         public int Hours { get; set; }
         public string Status { get; set; }
         public string? Code_reviewer { get; set; }
-        public string? Code_deviation_count { get; set; }
-        public string? Bugs_count { get; set; }
-        public string? Remarks { get; set; }
     }
 }
