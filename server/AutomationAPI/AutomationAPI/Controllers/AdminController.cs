3109bd73fea88d8c9857a749c2dccda9a9ffd8c8
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2022-11-30 11:16:11 +0530

    Get api for admin

--- server/AutomationAPI/AutomationAPI/Controllers/AdminController.cs
+++ server/AutomationAPI/AutomationAPI/Controllers/AdminController.cs
@@ -9,6 +9,35 @@ namespace AutomationAPI.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
-      
+        private readonly IConfiguration _configuration;
+        public AdminController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //------get API for admin------//
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select * from dbo.StoryTrackerDB";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyConnectionString");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+
+        }
     }
 }
