6aa1fcb75aba88f3d08592cec94060b675f174d1
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2022-11-30 11:19:28 +0530

    Get api for user

--- server/AutomationAPI/AutomationAPI/Controllers/UserController.cs
+++ server/AutomationAPI/AutomationAPI/Controllers/UserController.cs
@@ -10,6 +10,33 @@ namespace AutomationAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-       
+        private readonly IConfiguration _configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("{Emp_id}")]
+        public IActionResult GetEmp(int Emp_id)
+        {
+
+            string query2 = $"select * from StoryTrackerDB where Name in (select Emp_name from Emp_details  where Emp_id ={Emp_id})";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyConnectionString");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query2, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
     }
 }
