10cc70bbc678a7739651218c0c72f5a1a66432f8
Shreesh <shreesh.dutta@gmail.com>
2022-12-02 11:54:54 +0530

    Homepage commit 2

--- client/src/components/HomePage/HomePageMainComponent/HomePageMainComponent.jsx
+++ client/src/components/HomePage/HomePageMainComponent/HomePageMainComponent.jsx
@@ -4,6 +4,55 @@ import bglogo from '../../../assests/homeLogo.png';
 import bgHomePicture from '../../../assests/bg-image.jpg';
 
 function HomePageMainComponent() {
+  const [myData, setmyData] = useState();
+
+  useEffect(() => {
+    axios.post('https://localhost:7040/api/Emp', myData);
+    // .then((res)=> setmyData(res.data)
+    // );
+  });
+
+  <script
+    type="text/javascript"
+    src="https://alcdn.msauth.net/lib/1.3.0/js/msal.js"
+  ></script>;
+  async function run() {
+    console.log('running...');
+    const config = {
+      auth: {
+        clientId: 'addbbf7a-a97c-40e0-a662-37646b433007',
+        authority: 'https://login.microsoftonline.com/common/',
+        redirectUri: 'http://localhost:3000/Home'
+      }
+    };
+    var client = new Msal.UserAgentApplication(config);
+    var request = {
+      scopes: ['user.read']
+    };
+    let loginResponse = await client.loginPopup(request);
+    // console.dir(loginResponse);
+    let tokenResponse = await client.acquireTokenSilent(request);
+    // console.dir(tokenResponse);
+    let payload = await fetch('https://graph.microsoft.com/beta/me', {
+      headers: {
+        Authorization: 'Bearer ' + tokenResponse.accessToken
+      }
+    });
+    let json = await payload.json();
+    setmyData(() => {
+      return {
+        emp_id: json.employeeId,
+        emp_name: json.displayName,
+        emp_designation: json.jobTitle
+      };
+    });
+    //console.log(json.displayName,json.employeeId,json.jobTitle);
+  }
+
+  useEffect(() => {
+    myData && alert('Hello ' + myData.emp_name);
+  }, [myData]);
+
   return (
     <div className={css.home}>
       <img className={css.bgHomePicture} src={bgHomePicture}></img>
