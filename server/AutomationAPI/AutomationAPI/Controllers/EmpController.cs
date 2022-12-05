12ba8cc0294e1d1e2b2b22da4a224eed6c28b91e
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2022-12-05 12:26:06 +0530

    post api for window authentication

--- server/AutomationAPI/AutomationAPI/Controllers/EmpController.cs
+++ server/AutomationAPI/AutomationAPI/Controllers/EmpController.cs
@@ -10,6 +10,31 @@ namespace AutomationAPI.Controllers
     [ApiController]
     public class EmpController : ControllerBase
     {
-       
+        private readonly IConfiguration _configuration;
+        public EmpController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        public JsonResult Post([FromBody] Employee us)
+        {
+            string query1 = $"insert into dbo.CodeReview values(" + us.Emp_id + "," +
+                "'" + us.Emp_name + "', '" + us.Emp_designation + "')";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyConnectionString");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query1, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
     }
 }
