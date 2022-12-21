475d124a6624ea8a33268e06ce0c9c0432fa96f7
Shreesh <shreesh.dutta@gmail.com>
2022-12-21 15:55:03 +0530

    Button Base Structure

--- client/src/Pages/TablePage.jsx
+++ client/src/Pages/TablePage.jsx
@@ -5,73 +5,17 @@ import css from '../../src/Pages/TablePage.css'
 import bgHomePicture from '../assests/bg-image.jpg';
 import { ButtonComponent } from '../components/ButtonComponent/ButtonComponent';
 
-
- 
-
-  const add = () =>  {
-    function openForm() {
-      document.getElementById("myForm").style.display = "block";
-    }
-    
-    function closeForm() {
-      document.getElementById("myForm").style.display = "none";
-    }
-
-    return 
-    <form className={css.showForm}>
-      <h1>Column Name</h1>
-  
-      <label for="email"><b>Email</b></label>
-      <input type="text" placeholder="Enter Email" name="email" required></input>
-  
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required></input>
-  
-      <button type="submit" class="btn">Login</button>
-      <button type="button" class="btn cancel" onclick="closeForm()">Submit</button>
-    </form>
-    
-    }
-    
-
 function TablePage() {
-  const [openForm, setOpenForm] = useState(false);
-  return (
-    <div className={css.tabb}>
+  const [open, setOpen] = React.useState(false);
+  return (    
+    <>  
       <Navbar/>
             <ButtonComponent
                 cname={css.button1}
-                value="Add table"
-                openForm = {openForm}
-                setOpenForm = {setOpenForm}
+                value="Actions"
             />
-        <div className={openForm ? css.showForm : css.hideForm }>
-          {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-          <TextField id="filled-basic" label="Filled" variant="filled" />
-          <TextField id="standard-basic" label="Standard" variant="standard" /> */}
-          <form className={css.tableForm}>
-              <label>
-                Enter Table Name:
-                <input type="text" name="Table Name" />
-              </label>
-              <label>
-                Enter Column 1 Name:
-                <input type="text" name="Column 1 Name" />
-              </label>
-              <label>
-                Enter Column 2 Name:
-                <input type="text" name="Column 2 Name" />
-              </label>
-              <label>
-                Enter Column 3 Name:
-                <input type="text" name="Column 3 Name" />
-              </label>
-              <input type="submit" value="Add" />
-          </form>
-        </div>
       <Tables />
-      
-    </div>
+      </>
   )
 }
 
--- client/src/components/ButtonComponent/ButtonComponent.jsx
+++ client/src/components/ButtonComponent/ButtonComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const ButtonComponent = props => {
   return (
     <>
-      <button className={props.cname} type="submit" onClick={props.run ? props.run : (() => {props.setOpenForm(!props.openForm)})}>
+      <button className={props.cname} type="submit" onClick={props.run ? props.run : ''}>
         {' '}
         {props.value}{' '}
       </button>
